9e4a30568c582c97967bad0558a2d17f0b991e3b
tripercy <percytri37@gmail.com>
2024-06-14 13:50:49 +0700

    Changed top-nav border colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import LeftPane from './homepage/LeftPane';
 import TopNav from './nav/TopNav';
+import UserNav from './nav/UserNav';
 
 function App() {
   return (
     <div className="App">
-          <LeftPane />
       <body>
         <TopNav className="icon" />
+        <LeftPane />
+        <UserNav />
       </body>
     </div>
   );
---
+++ src/nav/UserNav.js
@@ -0,0 +1,9 @@
+
+
+export default function UserNav() {
+    return (
+        <div className="user-nav">
+
+        </div>
+    );
+}
