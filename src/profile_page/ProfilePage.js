daeaef4bfabab91c17eb0019ff15037c465c4355
tripercy <percytri37@gmail.com>
2024-06-19 23:34:25 +0700

    Finished contribution graph

--- src/profile_page/ProfilePage.js
+++ src/profile_page/ProfilePage.js
@@ -1,6 +1,7 @@
 import TopNav from "../nav/TopNav";
 import UserNav from "../nav/UserNav";
 import LeftNav from "../nav/LeftNav";
+import GitHubCalendar from 'react-github-calendar';
 
 import './ProfilePage.css';
 import ProfileOverview from "./ProfileOverview";
@@ -14,8 +15,13 @@ export default function ProfilePage() {
             <LeftNav />
 
             <div className="profile-content">
-                <ProfileOverview />
-                <PopularRepos />
+                <div className="profile-left">
+                    <ProfileOverview />
+                </div>
+                <div className="profile-right">
+                    <PopularRepos />
+                    <GitHubCalendar username="tripercy" />
+                </div>
             </div>
         </div>
     );
